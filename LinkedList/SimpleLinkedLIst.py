cb5cc3a21953a7d102970ac675b54148dfb78808
aashay4123 <aashayphirke5798@gmail.com>
2020-09-17 22:37:57 +0530

    update

--- LinkedList/SimpleLinkedLIst.py
+++ LinkedList/SimpleLinkedLIst.py
@@ -84,27 +84,106 @@ class SingleLinkedList:
         if k == self.start.info:
             temp = Node(data)
             temp.link = self.start
+            self.start = temp
+            return
+        p = self.start
+        while p.link is not None:
+            if p.link.info == k:
+                break
+            p = p.link
+        if p.link is None:
+            print(k, "is not present in the list")
+        else:
+            temp = Node(data)
+            temp.link = p.link
+            p.link = temp
 
     def insert_at_position(self, data, k):
-        pass
+        if k == 1:
+            temp = Node(data)
+            temp.link = self.start
+            self.start = temp
+            return
+        p = self.start
+        i = 1
+        while i < k-1 and p is not None:
+            p = p.link
+            i += 1
+        if p is None:
+            print("you can insert upto position ", i)
+        else:
+            temp = Node(data)
+            temp.link = p.link
+            p.link = temp
 
     def delete_node(self, data):
-        pass
+        if self.start is None:
+            print("List is empty")
+            return
+        p = self.start
+        while p.link is not None:
+            if p.link.info == data:
+                break
+            p = p.link
+        if p.link is None:
+            print("element ", data, " not found in list")
+        else:
+            p.link = p.link.link
 
     def delete_first(self):
-        pass
+        if self.start:
+            return
+        self.start = self.start.link
 
     def delete_last(self):
-        pass
+        if self.start is None:
+            return
+        if self.start.link is None:
+            self.start = None
+            return
+        p = self.start
+        while p.link is not None:
+            p = p.link
+        p = None
 
     def reverse_list(self):
-        pass
+        prev = None
+        p = self.start
+        while p is not None:
+            next = p.link
+            p.link = prev
+            prev = p
+            p = next
+        self.start = prev
 
     def bubble_sort_exdata(self):
-        pass
+        end = None
+        while end != self.start.link:
+            p = self.start
+            while p.link != end:
+                q = p.link
+                if(p.info > q.info):
+                    p.info, q.info = q.info, p.info
+                p = p.link
+            end = p
 
     def bubble_sort_exlinks(self):
-        pass
+        end = None
+        while end != self.start.link:
+            r = p = self.start
+            while p.link != end:
+                q = p.link
+                if(p.info > q.info):
+                    p.link = q.link
+                    q.link = p
+                    if p != self.start:
+                        r.link = q
+                    else:
+                        self.start = q
+                    p, q = q, p
+                r = p
+                p = p.link
+            end = p
 
     def find_cycle(self):
         pass
@@ -136,6 +215,15 @@ data = int(input("Enter the element:"))
 list.create_list()
 list.insert_start(data)
 list.insert_end(data)
+list.insert_after(122, data)
+list.insert_before(152, data)
 list.count_nodes()
+list.insert_at_position(443, 3)
+list.reverse_list()
+list.delete_first()
+list.delete_last()
+list.delete_node(data)
+# list.bubble_sort_exdata()/
+# list.bubble_sort_exlinks()
 list.display_list()
 list.search(data)
